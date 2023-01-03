6a24db090ef828a2b3d843bba31cd833e925075b
AndrikSokol <AndrikSokol31@gmail.com>
2023-01-04 01:31:41 +0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace SAOD_kyrsach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_add_font = new System.Windows.Forms.Button();
+            this.Btn_del = new System.Windows.Forms.Button();
+            this.Btn_Find = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Btn_add_back = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Btn_add_font
+            // 
+            this.Btn_add_font.Location = new System.Drawing.Point(12, 263);
+            this.Btn_add_font.Name = "Btn_add_font";
+            this.Btn_add_font.Size = new System.Drawing.Size(78, 45);
+            this.Btn_add_font.TabIndex = 0;
+            this.Btn_add_font.Text = "Добавить \r\nв начало";
+            this.Btn_add_font.UseVisualStyleBackColor = true;
+            this.Btn_add_font.Click += new System.EventHandler(this.Btn_add_font_Click);
+            // 
+            // Btn_del
+            // 
+            this.Btn_del.Location = new System.Drawing.Point(180, 263);
+            this.Btn_del.Name = "Btn_del";
+            this.Btn_del.Size = new System.Drawing.Size(78, 45);
+            this.Btn_del.TabIndex = 1;
+            this.Btn_del.Text = "Удалить \r\nИмя";
+            this.Btn_del.UseVisualStyleBackColor = true;
+            this.Btn_del.Click += new System.EventHandler(this.Btn_del_Click);
+            // 
+            // Btn_Find
+            // 
+            this.Btn_Find.Location = new System.Drawing.Point(264, 263);
+            this.Btn_Find.Name = "Btn_Find";
+            this.Btn_Find.Size = new System.Drawing.Size(78, 45);
+            this.Btn_Find.TabIndex = 2;
+            this.Btn_Find.Text = "Найти имя\r\n";
+            this.Btn_Find.UseVisualStyleBackColor = true;
+            this.Btn_Find.Click += new System.EventHandler(this.Btn_Find_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(55, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(215, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Поиск";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(274, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Btn_add_back
+            // 
+            this.Btn_add_back.Location = new System.Drawing.Point(96, 263);
+            this.Btn_add_back.Name = "Btn_add_back";
+            this.Btn_add_back.Size = new System.Drawing.Size(78, 45);
+            this.Btn_add_back.TabIndex = 7;
+            this.Btn_add_back.Text = "Добавить \r\nв конец\r\n";
+            this.Btn_add_back.UseVisualStyleBackColor = true;
+            this.Btn_add_back.Click += new System.EventHandler(this.Btn_add_back_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 43);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Очистить список";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(603, 214);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(639, 319);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_add_back);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Btn_Find);
+            this.Controls.Add(this.Btn_del);
+            this.Controls.Add(this.Btn_add_font);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Кольцевой список";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_add_font;
+        private System.Windows.Forms.Button Btn_del;
+        private System.Windows.Forms.Button Btn_Find;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Btn_add_back;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SAOD_kyrsach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        unsafe public class Node
+        {
+            public Node(string data)
+            {
+                Data = data;
+            }
+            public string Data { get; set; }
+            public Node Next { get; set; }
+
+        }
+        public class Circle_Link_list
+        {
+            Node head;
+            Node last;
+            int count;
+            int pos_of_del;
+            int pos_of_find;
+
+            public string show()
+            {
+                
+                string text ="";
+                Node current = head;
+                int i = 0;
+                 
+                while(i<count)
+                {
+                    Node a = current;
+                    Node r = current;
+                    var ad = a.Next?.GetHashCode()??0;
+                    text += "Элемент " + (i + 1).ToString()+ " " +current.Data +  "\tАдрес элемента: " + r.GetHashCode() + " Указатель на след: " + ad +"\n";
+                    current = current.Next;
+                    i++;
+                    
+                }
+
+                return text;
+            }
+        public void Add_back(string data)
+            {
+                Node node = new Node(data);
+                if (head == null)
+                {
+                    head = node;
+                    last = node;
+                    last.Next = head;
+                }
+                else
+                {
+                    node.Next = head;
+                    last.Next = node;
+                    last = node;
+                }
+                count++;
+            }
+            public void Add_front(string data)
+            {
+                Node node = new Node(data);
+                if (head == null)
+                {
+                    head = node;
+                    last = node;
+                    last.Next = head;
+                }
+                else
+                {
+                    last.Next = node;
+                    node.Next = head;
+                    head = node;
+                }
+                count++;
+            }
+            
+            public bool Remove(string data)
+            {
+                pos_of_del = 0;
+                Node current = head;
+                Node previous = null;
+
+                if (IsEmpty)
+                    return false;
+                do
+                {
+                    if (current.Data.Equals(data))
+                    {
+                        if (previous != null)
+                        {
+                            previous.Next = current.Next;
+                            if (current == last)
+                                last = previous;
+                        }
+                        else
+                        {
+                            if (count == 1)
+                                head = last = null;
+                            else
+                            {
+                                head = current.Next;
+                                last.Next = current.Next;
+                            }
+
+                        }
+                        count--;
+                        return true;
+
+                    }
+                    pos_of_del++;
+                    previous = current;
+                    current = current.Next;
+
+                } while (current != head);
+                return false;
+            }
+            public int Count
+            { get { return count; } }
+            public int Pos_of_del
+            { get { return pos_of_del; } }
+            public int Pos_of_find
+            { get { return pos_of_find; } }
+            public bool IsEmpty
+            { get { return count == 0; } }
+
+            public void Clear()
+            {
+                head = null;
+                last = null;
+                count = 0;
+            }
+            public bool Contains(string data)
+            {
+                Node current = head;
+                pos_of_find = 0;
+                if (current == null)
+                    return false;
+                do
+                {
+                    if (current.Data.Equals(data))
+                        return true;
+                    current = current.Next;
+                    pos_of_find++;
+                }
+                while (current != head);
+                return false;
+            }
+        }
+
+        Circle_Link_list Circle_list = new Circle_Link_list();
+
+        async void Add_color(int numb)
+        {
+                if (numb == 1)
+                {
+                    string line = richTextBox1.Lines[0];
+                    int start = richTextBox1.GetFirstCharIndexFromLine(0);
+                    int end = line.Length;
+                    richTextBox1.SelectionStart = start;
+                    richTextBox1.SelectionLength = end;
+                    richTextBox1.SelectionBackColor = Color.Green;
+                    await Task.Delay(2000);
+                    richTextBox1.SelectionBackColor = Color.White;
+                }
+                if (numb ==2)
+                {
+                    int count = Circle_list.Count-1;
+                    string line = richTextBox1.Lines[count];
+                    int start = richTextBox1.GetFirstCharIndexFromLine(count);
+                    int end = line.Length;
+                    richTextBox1.SelectionStart = start;
+                    richTextBox1.SelectionLength = end;
+                    richTextBox1.SelectionBackColor = Color.Green;
+                    await Task.Delay(2000);
+                    richTextBox1.SelectionBackColor = Color.White;
+                }
+                if (numb ==3)
+                {
+                    int pos = Circle_list.Pos_of_del;
+                    string line = richTextBox1.Lines[pos];
+                    int start = richTextBox1.GetFirstCharIndexFromLine(pos);
+                    int end = line.Length;
+                    richTextBox1.SelectionStart = start;
+                    richTextBox1.SelectionLength = end;
+                    richTextBox1.SelectionBackColor = Color.Red;
+                    await Task.Delay(2000);
+                    richTextBox1.SelectionBackColor = Color.White;
+                    richTextBox1.Text = Circle_list.show();
+                }
+            if (numb == 4)
+            {
+                int pos = Circle_list.Pos_of_find;
+                string line = richTextBox1.Lines[pos];
+                int start = richTextBox1.GetFirstCharIndexFromLine(pos);
+                int end = line.Length;
+                richTextBox1.SelectionStart = start;
+                richTextBox1.SelectionLength = end;
+                richTextBox1.SelectionBackColor = Color.Yellow;
+                await Task.Delay(2000);
+                richTextBox1.SelectionBackColor = Color.White;
+
+            }
+
+        }
+        private void Btn_add_font_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            string data = textBox1.Text;
+            Circle_list.Add_front(data);
+            richTextBox1.Text = Circle_list.show();
+            Add_color(1);
+            textBox1.Clear();
+        }
+
+        private void Btn_add_back_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            string data = textBox1.Text;
+            Circle_list.Add_back(data);
+            richTextBox1.Text = Circle_list.show();
+            Add_color(2);
+            textBox1.Clear();
+        }
+
+        private void Btn_del_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            string data = textBox1.Text;
+            if (Circle_list.Remove(data))
+            {
+                Add_color(3);
+                textBox1.Clear();
+            }
+            else
+                MessageBox.Show("Такое имя не существует");
+
+            
+
+        }
+
+        private void Btn_Find_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+                return;
+            string data = textBox2.Text;
+            if (Circle_list.Contains(data))
+            {
+                Add_color(4);
+                textBox2.Clear();
+            }
+            else
+                MessageBox.Show("Такое имя не найдено");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Circle_list.Clear();
+            richTextBox1.Clear();
+            richTextBox1.ScrollBars = RichTextBoxScrollBars.None;
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
